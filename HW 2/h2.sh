3a4a3dbeaa37fc893e4be3ea5660d52ef5938dc4
Caitlyn Chau <caitlynchau2@gmail.com>
2022-03-11 20:55:35 -0800

    modify script

--- HW 2/h2.sh
+++ HW 2/h2.sh
@@ -1,7 +1,7 @@
-echo 'starting h2.sql'
+echo '[h2.sql] creating triggers'
 db2 -td"^" -f h2.sql > res.out
-echo 'finished h2.sql'
+echo '[h2.sql] finished creating triggers'
 
-echo 'starting test.sql'
+echo '[test.sql] running test cases'
 db2 -td"^" -f test.sql >> res.out
-echo 'finished test.sql'
+echo '[test.sql] finished running test cases'
